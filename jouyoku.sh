037557dff1b81118d6fda5cf9d315bb1486b0919
Cid Huang <cid@msbt.com.tw>
2020-12-03 09:22:03 +0800

    add: monitoring api

--- jouyoku.sh
+++ jouyoku.sh
@@ -1,4 +1,5 @@
 ./watch-pug.sh msbt/pug/ ../admin/test-msbt/w4/ &
 ./watch-sass.sh msbt/scss/ ../admin/test-msbt/w4/ &
 ./watch-prettier.sh msbt/js/ ../admin/test-msbt/w4/ &
-./watch-prettier.sh msbt/php/cid.lib/ ../admin/test-msbt/w4/cid.lib &
+./watch-prettier.sh msbt/php/cid.lib/ ../admin/test-msbt/w4/cid.lib/ &
+./watch-prettier.sh msbt/php/api/ ../admin/test-msbt/w4/web/msbt_admin/cid/api/ &
