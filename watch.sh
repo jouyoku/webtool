63f19e25b79eb6486db7d14e866f871cf4dbb08c
Cid Huang <cid@msbt.com.tw>
2021-01-05 16:49:20 +0800

    change: watch.sh for coupon api

--- watch.sh
+++ watch.sh
@@ -2,4 +2,5 @@
 ./watch-sass.sh msbt/scss/ ../admin/test-msbt/w4/ &
 ./watch-prettier.sh msbt/js/ ../admin/test-msbt/w4/ &
 ./watch-prettier.sh msbt/php/cid.lib/ ../admin/test-msbt/w4/cid.lib/ &
+./watch-prettier.sh msbt/php/web.cid.api/ ../admin/test-msbt/w4/web/cid/api/ &
 ./watch-prettier.sh msbt/php/api/ ../admin/test-msbt/w4/web/msbt_admin/cid/api/ &
