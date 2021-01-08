400a503aa2cbb1162023c23a4efb0485d831a4d7
Cid Huang <cid@msbt.com.tw>
2021-01-08 15:09:45 +0800

    add: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 ./deploy-sass.sh msbt/scss/ ../admin/test-msbt/w4/
 ./deploy-terser.sh msbt/js/ ../admin/test-msbt/w4/
 ./deploy-prettier.sh msbt/php/cid.lib/ ../admin/test-msbt/w4/cid.lib/
+./deploy-prettier.sh msbt/php/web.cid.api/ ../admin/test-msbt/w4/web/cid/api/
 ./deploy-prettier.sh msbt/php/api/ ../admin/test-msbt/w4/web/msbt_admin/cid/api/
